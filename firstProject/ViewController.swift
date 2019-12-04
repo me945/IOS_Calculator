ec0c03a98ec223849dcd8fdbd7c445e562feab86
Mahmoud <potatosheads@gmail.com>
2019-12-04 11:13:41 +0400

    IOS Usless Calculator Like Me

---
+++ firstProject/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  firstProject
+//
+//  Created by student on 04/12/2019.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var TextView1: UITextField!
+    
+    
+    
+    @IBAction func Number1(_ sender: UIButton) {
+        
+    }
+    @IBAction func Number2(_ sender: UIButton) {
+    }
+    @IBAction func Number3(_ sender: UIButton) {
+    }
+    @IBAction func Number4(_ sender: UIButton) {
+    }
+    @IBAction func Number5(_ sender: UIButton) {
+    }
+    @IBAction func Number6(_ sender: UIButton) {
+    }
+    @IBAction func Number7(_ sender: UIButton) {
+    }
+    @IBAction func Number8(_ sender: UIButton) {
+    }
+    @IBAction func Number9(_ sender: UIButton) {
+    }
+    @IBAction func Addition(_ sender: UIButton) {
+    }
+    @IBAction func Subtraction(_ sender: UIButton) {
+    }
+    @IBAction func Multiplication(_ sender: UIButton) {
+    }
+    @IBAction func equalButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var a : Int
+        var b : Int
+        init(a:Int, b:Int)
+        {
+            self.a = a
+            self.b = b
+            super.init()
+        }
+        
+    }
+
+}
+
